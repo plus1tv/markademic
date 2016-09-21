44b0be63065df5be90af8c05f3ec61eeb59434aa
Alain Galvan <hi@alain.xyz>
2016-09-21 10:05:18 -0400

    Fixed issue with type definitions

--- src/markademic.ts
+++ src/markademic.ts
@@ -5,9 +5,9 @@ import {symbolRender, citationsRender} from './jsonrender';
 
 export type Config = {
   input: string, 
-  citations: any, 
-  symbols: {[latexSymbol: string]: {type: string, description: string}},
-  rerouteLinks: ((str: string) => string)
+  citations?: any, 
+  symbols?: {[latexSymbol: string]: {type: string, description: string}},
+  rerouteLinks?: ((str: string) => string)
 };
 
 /**
