60c349c7c624c921a726827dcfb7c082be6d964a
Alain Galvan <hi@alain.xyz>
2022-02-07 20:01:59 -0500

    Added more aliases for assembly languages

--- src/remarkable.ts
+++ src/remarkable.ts
@@ -1,15 +1,21 @@
 import * as Remarkable from "remarkable";
 const remark: any = Remarkable as any;
 import hljs from "highlight.js";
-const { getLanguage, highlight, highlightAuto, registerLanguage, registerAliases } = hljs;
-import hlsl from './languages/hlsl';
-import cpp from './languages/cpp';
-import wgsl from './languages/wgsl';
-hljs.registerLanguage('hlsl', hlsl);
-hljs.registerLanguage('cpp', cpp);
-hljs.registerLanguage('wgsl', wgsl);
-hljs.registerAliases('asm', {languageName: 'x86asm'});
-hljs.registerAliases('msl', {languageName: 'cpp'});
+const {
+  getLanguage,
+  highlight,
+  highlightAuto,
+  registerLanguage,
+  registerAliases,
+} = hljs;
+import hlsl from "./languages/hlsl";
+import cpp from "./languages/cpp";
+import wgsl from "./languages/wgsl";
+registerLanguage("hlsl", hlsl);
+registerLanguage("cpp", cpp);
+registerLanguage("wgsl", wgsl);
+registerAliases(["asm", "amdil", "ptx"], { languageName: "x86asm" });
+registerAliases("msl", { languageName: "cpp" });
 
 const remarkable = new remark.Remarkable({
   html: true,
